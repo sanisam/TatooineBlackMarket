f0da70969d52684a561f1583731c03c141665d14
Roland Schenk <roland.schenk@hoermoos.ch>
2022-05-08 08:30:29 +0200

    order button styling

--- Dit.Umb9.Mutobo.Web/wwwroot/web-components-cms-template-base/src/es/components/organisms/OrderButton.js
+++ Dit.Umb9.Mutobo.Web/wwwroot/web-components-cms-template-base/src/es/components/organisms/OrderButton.js
@@ -30,17 +30,47 @@ export default class OrderButton extends Shadow() {
     }
 
     renderCSS() {
+        this.css = `
+            @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css");
+
+            :host div.order-button {
+                padding: 0.5em;
+                color: white;
+                background-color: rgba(0,0,0,0.7);
+                margin: 1em;
+               
+            
+
+            }
+
+            :host div.order-button:hover {
+                color: black;
+                background-color: rgba(255,255,255,0.7);
+                
+                
+            
+            }
+
+            :host div.order-button  * { margin: 0 0.25em;}
+        
+        `;
+       
 
     }
 
 
     fireEvent(event) {
 
-       
+        debugger;
+
+        let productId = event.target.getAttribute('data-id') ?? event.target.parentNode.getAttribute('data-id');
         const customEvent = new CustomEvent('addToBasket',
             {
+
+
+
                 detail: {
-                    productId: event.target.getAttribute('data-id')             
+                    productId: productId,           
                 },
                 bubbles: true,
                 cancelable: false,
@@ -56,12 +86,20 @@ export default class OrderButton extends Shadow() {
     renderHTML() {
 
         this.container = document.createElement('DIV');
-        this.button = document.createElement('BUTTON');
-        
+        this.button = document.createElement('div');
+        this.button.className = 'order-button';
+        this.buttonIcon = document.createElement('i');
+        this.buttonIcon.className = 'bi bi-bag-fill';
+        this.buttonText = document.createElement('span');
+        this.buttonText.innerText = 'In den Warenkorb';
+        this.button.appendChild(this.buttonIcon);
+        this.button.appendChild(this.buttonText);
+
         this.button.setAttribute('data-id', this.getAttribute('product-id'));
+     
         
         this.button.addEventListener('click', event => this.fireEvent(event));
-        this.button.textContent = 'Klick mich';
+    
         this.container.appendChild(this.button);
         this.html = this.container;
     }
